1757a85c0f2a6417f7dc83163f0b807a8c5338cc
csj <15778535572@139.com>
2023-03-03 05:08:59 +0800

    update

--- jsa/build/main.js
+++ jsa/build/main.js
@@ -1,4 +1,4 @@
 //这个文件由index.html包含
-document.write("<script language='javascript' src='js/util.js'></script>");
-document.write("<script language='javascript' src='js/ribbon.js'></script>");
-document.write("<script language='javascript' src='js/systemdemo.js'></script>");
+document.write("<script language='javascript' src='https://caisijian.github.io/publish_test/jsa/build/js/util.js'></script>");
+document.write("<script language='javascript' src='https://caisijian.github.io/publish_test/jsa/build/js/ribbon.js'></script>");
+document.write("<script language='javascript' src='https://caisijian.github.io/publish_test/jsa/build/js/systemdemo.js'></script>");
