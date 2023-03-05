3a08829ac6a2a6f6c4388d1cc4614fc99a0b0a32
csj <15778535572@139.com>
2023-03-05 14:46:49 +0800

    delete之前

--- server.mjs
+++ server.mjs
@@ -4,8 +4,8 @@ const app = express();
 
 // 在这里，调用 express.static() 方法，快速的对外提供静态资源
 // app.use('/files', express.static('./*'))
-app.use(express.static('./jsa/build/'));
+app.use(express.static('D:\\codeAdministration\\gitee\\test\\wordReserve'));
 
-app.listen(80, () => {
+app.listen(8080, () => {
     console.log('express server running at http://127.0.0.1');
 });
