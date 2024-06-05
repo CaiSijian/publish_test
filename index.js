bd21e7bc23043dee759960961d0d1e10403eef6d
CaiSijian <112552123+CaiSijian@users.noreply.github.com>
2024-06-06 04:20:51 +0800

    update

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 /**@type {HTMLDivElement} */
 const h1 = document.querySelector('h1')
 const h2 = document.querySelector('h2')
-h1.innerText='0.1.3'
-h2.innerText='current event name'
+h1.innerText = '0.1.3'
+h2.innerText = 'current event name'
 const input = document.querySelector('input')
 const textarea = document.querySelector('textarea')
 input.addEventListener('keydown', handler)
@@ -13,8 +13,9 @@ window.addEventListener('keydown', handler)
  * @param {MouseEvent} event 
 */
 function handler(event) {
-    h2.innerText=this===window ? 'window' :'input'
-    const { ctrlKey,shiftKey,altKey,metaKey, code,key,keyCode} = event
-    const info={ ctrlKey,shiftKey,altKey,metaKey, code,key,keyCode}
-    textarea.value = JSON.stringify(info,undefined,4)
+    h2.innerText = this === window ? 'window' : 'input'
+    const eventName = this === window ? 'window' : 'input'
+    const { ctrlKey, shiftKey, altKey, metaKey, code, key, keyCode } = event
+    const info = { ctrlKey, shiftKey, altKey, metaKey, code, key, keyCode }
+    textarea.value = `${eventName} ${JSON.stringify(info, undefined, 4)} \n\r${textarea?.value}`
 }
