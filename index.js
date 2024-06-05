90395d19a3ac6ae15369d4f68fdbbdc91ec87bd3
CaiSijian <112552123+CaiSijian@users.noreply.github.com>
2024-06-06 02:23:49 +0800

    feat:测试依旧

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-window.addEventListener('DOMContentLoaded',() => {
-    const input=document.querySelector('input')
-    input.addEventListener('keydown',(event) => {
-        if(event.code==='Enter'){
-            alert(event.code)
-        }
+// @ts-check
+window.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('input')
+    input.addEventListener('keydown', (event) => {
+        const message = `${event.code}  ${event.key}`
+        alert(message)
     })
 })
-// @ts-check
 window.addEventListener('keydown', (event) => {
-    alert(event.code)
+    const message = `${event.code}  ${event.key}`
+    alert(message)
 })
