b5ce0fa857df3fc01f60d349ead473718713b1ff
CaiSijian <15778535572@139.com>
2024-06-05 09:57:53 +0800

    feat:测试移动端表单问题

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+window.addEventListener('DOMContentLoaded',() => {
+    const input=document.querySelector('input')
+    input.addEventListener('keydown',(event) => {
+        if(event.code==='Enter'){
+            alert(event.code)
+        }
+    })
+})
 // @ts-check
 window.addEventListener('keydown', (event) => {
     alert(event.code)
