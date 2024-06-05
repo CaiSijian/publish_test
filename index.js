d07424b141d502cb02144e74173ee06916da8af5
CaiSijian <112552123+CaiSijian@users.noreply.github.com>
2024-06-06 03:26:32 +0800

    update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // @ts-check
 /**@type {HTMLDivElement} */
 const h1 = document.querySelector('h1')
-h1.innerText='0.1.1'
+h1.innerText='0.1.2'
 const input = document.querySelector('input')
 const textarea = document.querySelector('textarea')
 input.addEventListener('keydown', (event) => {
-    const message = `${event.code}   ${event.key}`
+    const message = `input event
+${event.code}   ${event.key}`
     textarea.value = JSON.stringify(message)
 })
 window.addEventListener('keydown', (event) => {
-    const message = `${event.code}   ${event.key}`
+    const message = `window event
+${event.code}   ${event.key}`
     textarea.value = JSON.stringify(message)
 })
